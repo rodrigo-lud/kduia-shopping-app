34363d2a9a0cf8188c47eb7361781e3408b6f14a
Rodrigo <rodrigo.lud@gmail.com>
2023-08-21 14:57:05 -0400

    changes made from theia lab environment

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,6 +2,30 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+  const {dispatch } = useContext(AppContext);
+
+    const changeLocation = (val)=>{
+            dispatch({
+                type: 'CHG_LOCATION',
+                payload: val,
+            })
+    }
+    
+
+  return (
+        <div className='alert alert-secondary'> Location {
+      <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
+        <option value="£">Uk(£)</option>
+        <option value="₹">India(₹)</option>
+        <option value="€">Europe(€)</option>
+        <option value="CAD">Canada(CAD)</option>
+        <option value="US$">Unitad States(US$)</option>
+        <option value="R$">Brazil(R$)</option>
+
+      </select>	
+      }	
+    </div>
+    );
 };
 
 export default Location;
